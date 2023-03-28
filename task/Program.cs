b6535c6dfdbff24c5274776dd5a881f6b11784b2
Liliya Bezmaternyh <Liliya1102@yandex.ru>
2023-03-28 23:24:50 +0500

    решение

--- task/Program.cs
+++ task/Program.cs
@@ -2,3 +2,30 @@
 // длина которых меньше или равна три символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
 // выполнения алгоритма.
+Console.Clear();
+
+string[] array1 = new string[4] {"hello", "2", "world", ";-)"};
+Console.WriteLine(String.Join(", ", array1));
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
